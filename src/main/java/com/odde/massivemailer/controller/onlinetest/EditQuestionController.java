b99118614ca803fc5f0137e79f565bf1d2f2f4b5
松本 圭威 <kei.matsumoto@bizreach.co.jp>
2018-11-28 17:49:15 +0900

    [GREEN] make edit question test class empty

---
+++ src/main/java/com/odde/massivemailer/controller/onlinetest/EditQuestionController.java
@@ -0,0 +1,4 @@
+package com.odde.massivemailer.controller.onlinetest;
+
+public class EditQuestionController {
+}
--- src/test/java/com/odde/massivemailer/controller/onlinetest/EditQuestionControllerTest.java
+++ src/test/java/com/odde/massivemailer/controller/onlinetest/EditQuestionControllerTest.java
@@ -2,6 +2,7 @@ package com.odde.massivemailer.controller.onlinetest;
 
 import com.odde.TestWithDB;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -18,5 +19,10 @@ public class EditQuestionControllerTest {
         this.controller = new EditQuestionController();
         this.request = new MockHttpServletRequest();
         this.response = new MockHttpServletResponse();
+    }
+
+    @Test
+    public void doPostEditQuestion() throws Exception {
+
     }
 }
