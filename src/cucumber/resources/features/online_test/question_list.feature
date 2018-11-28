6b455af4e6f4144dcca88d735afe046292beb01e
尾上 貴子 <takako.onoue@bizreach.co.jp>
2018-11-28 16:05:50 +0900

    [Red] QuestionList一覧でクエスチョンがないときに落ちるテスト追加

--- src/cucumber/resources/features/online_test/question_list.feature
+++ src/cucumber/resources/features/online_test/question_list.feature
@@ -5,3 +5,15 @@ Feature: Question List
     Given Questionに一件も登録されていない
     When 左部のサイドバーのQuestionListをクリックしたとき
     Then QuestonList画面にヘッダのみ表示される
+
+  @now
+  Scenario: クエスチョンがある時の一覧の表示
+    Given Questionが10件登録されている、1件目に「Scrumの用語はどれか」というDescriptionで登録する
+    When 左部のサイドバーのQuestionListをクリックしたとき
+    Then 1件目のQuestionIDが"1"と表示しされている事
+    And QuestionListが10件表示され、Questionが1行ずつ図のように表示される
+    And 「QuestionID」「Description」「Edit」カラムが表示されている事
+    And 1件目のDescriptionに"Scrumの擁護はどれか？"と表示されるされている事
+    And 「Edit」というキャプションのボタンが1件毎に表示される事
+    And QuestionListの一番上にはQuestionIDの昇順で表示される事
+
--- src/test/java/steps/online/QuestionListSteps.java
+++ src/test/java/steps/online/QuestionListSteps.java
@@ -1,8 +1,9 @@
 package steps.online;
 
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import cucumber.api.java.en.Given;
 import steps.driver.WebDriverWrapper;
 import steps.site.MassiveMailerSite;
 
@@ -20,6 +21,7 @@ public class QuestionListSteps {
 
     @When("^左部のサイドバーのQuestionListをクリックしたとき$")
     public void 左部のサイドバーのQuestionListをクリックしたとき() {
+
         site.visit("onlinetest/question_list.jsp");
     }
 
@@ -27,4 +29,35 @@ public class QuestionListSteps {
     public void QuestonList画面にヘッダのみ表示される() {
         driver.pageShouldContain("QuestionID Description Edit");
     }
+
+
+    @Given("^Questionが(\\d+)件登録されている、(\\d+)件目に「Scrumの用語はどれか」というDescriptionで登録する$")
+    public void questionが件登録されている件目にScrumの用語はどれかというDescriptionで登録する(int arg0, int arg1) {
+    }
+
+    @Then("^(\\d+)件目のQuestionIDが\"([^\"]*)\"と表示しされている事$")
+    public void 件目のquestionidがと表示しされている事(int arg0, String arg1) {
+        driver.pageShouldContain("1 スクラム用語はどれか Edit");
+    }
+
+    @And("^QuestionListが(\\d+)件表示され、Questionが(\\d+)行ずつ図のように表示される$")
+    public void questionlistが件表示されQuestionが行ずつ図のように表示される(int arg0, int arg1)  {
+    }
+
+    @And("^「QuestionID」「Description」「Edit」カラムが表示されている事$")
+    public void questionidDescriptionEditカラムが表示されている事() {
+    }
+
+    @And("^(\\d+)件目のDescriptionに\"([^\"]*)\"と表示されるされている事$")
+    public void 件目のdescriptionにと表示されるされている事(int arg0, String arg1) {
+    }
+
+    @And("^「Edit」というキャプションのボタンが(\\d+)件毎に表示される事$")
+    public void editというキャプションのボタンが件毎に表示される事(int arg0) {
+    }
+
+    @And("^QuestionListの一番上にはQuestionIDの昇順で表示される事$")
+    public void questionlistの一番上にはquestionidの昇順で表示される事() {
+    }
+
 }
