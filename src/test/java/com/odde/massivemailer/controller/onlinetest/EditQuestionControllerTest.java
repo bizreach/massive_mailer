3a385214fc3b2ebca8746574f1a9d603967fdde4
松本 圭威 <kei.matsumoto@bizreach.co.jp>
2018-11-28 17:47:11 +0900

    [RED] create edit question test class

---
+++ src/test/java/com/odde/massivemailer/controller/onlinetest/EditQuestionControllerTest.java
@@ -0,0 +1,22 @@
+package com.odde.massivemailer.controller.onlinetest;
+
+import com.odde.TestWithDB;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+@RunWith(TestWithDB.class)
+public class EditQuestionControllerTest {
+
+    private EditQuestionController controller;
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+
+    @Before
+    public void setUpMockService() {
+        this.controller = new EditQuestionController();
+        this.request = new MockHttpServletRequest();
+        this.response = new MockHttpServletResponse();
+    }
+}
