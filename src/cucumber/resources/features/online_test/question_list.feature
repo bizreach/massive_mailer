f57080c053ea2523b52c43f7ce7b6e293c4bce52
尾上 貴子 <takako.onoue@bizreach.co.jp>
2018-11-28 10:58:23 +0900

    QuestionListのfeatureとstepの追加

---
+++ src/cucumber/resources/features/online_test/question_list.feature
@@ -0,0 +1,7 @@
+Feature: Question List
+
+
+  Scenario: Questionがない時の一覧表示
+    Given Questionに一件も登録されていない
+    When 左部のサイドバーのQuestionListをクリックしたとき
+    Then QuestonList画面にヘッダのみ表示される
---
+++ src/test/java/steps/online/QuestionListSteps.java
@@ -0,0 +1,26 @@
+package steps.online;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Given;
+
+public class QuestionListSteps {
+    @Given("^Questionに一件も登録されていない$")
+    public void questionに一件も登録されていない() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        // throw new PendingException();
+    }
+
+    @When("^左部のサイドバーのQuestionListをクリックしたとき$")
+    public void 左部のサイドバーのquestionlistをクリックしたとき() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        // throw new PendingException();
+    }
+
+    @Then("^QuestonList画面にヘッダのみ表示される$")
+    public void questonlist画面にヘッダのみ表示される() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        // throw new PendingException();
+    }
+}
