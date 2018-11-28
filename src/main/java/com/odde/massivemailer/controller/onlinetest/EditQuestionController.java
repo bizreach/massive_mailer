50a3d332c380dc0cce6745c824fac340a4f2967b
松本 圭威 <kei.matsumoto@bizreach.co.jp>
2018-11-28 17:53:31 +0900

    [GREEN] implements empty do get method

--- src/main/java/com/odde/massivemailer/controller/onlinetest/EditQuestionController.java
+++ src/main/java/com/odde/massivemailer/controller/onlinetest/EditQuestionController.java
@@ -1,4 +1,14 @@
 package com.odde.massivemailer.controller.onlinetest;
 
-public class EditQuestionController {
+import com.odde.massivemailer.controller.AppController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EditQuestionController extends AppController {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+    }
 }
