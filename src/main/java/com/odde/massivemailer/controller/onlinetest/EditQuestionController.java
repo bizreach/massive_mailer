74a6c7776c55634c6d733d12722331a5262da0cd
松本 圭威 <kei.matsumoto@bizreach.co.jp>
2018-11-29 11:23:04 +0900

    [GREEN] 編集画面にフォワードする実装を追加

--- src/main/java/com/odde/massivemailer/controller/onlinetest/EditQuestionController.java
+++ src/main/java/com/odde/massivemailer/controller/onlinetest/EditQuestionController.java
@@ -2,12 +2,20 @@ package com.odde.massivemailer.controller.onlinetest;
 
 import com.odde.massivemailer.controller.AppController;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet("/onlinetest/edit_question")
 public class EditQuestionController extends AppController {
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // request.setAttribute("sample", "value");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/onlinetest/edit_question.jsp");
+        dispatcher.forward(request, response);
     }
 }
