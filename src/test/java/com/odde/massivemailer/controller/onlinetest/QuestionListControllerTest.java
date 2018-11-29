8ea102fb57bea5d006c8073890465be7f74894b9
尾上 貴子 <takako.onoue@bizreach.co.jp>
2018-11-29 12:01:39 +0900

    [Red]クエスチョンリストのコントローラーの1つあるときテスト追加。

--- src/test/java/com/odde/massivemailer/controller/onlinetest/QuestionListControllerTest.java
+++ src/test/java/com/odde/massivemailer/controller/onlinetest/QuestionListControllerTest.java
@@ -1,15 +1,15 @@
 package com.odde.massivemailer.controller.onlinetest;
 
 import com.odde.TestWithDB;
-import com.odde.massivemailer.model.User;
+import com.odde.massivemailer.model.onlinetest.Question;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -21,8 +21,11 @@ public class QuestionListControllerTest {
     private final MockHttpServletResponse response = new MockHttpServletResponse();
 
     @Test
-    public void getRequest() throws ServletException, IOException {
+    public void doGetQuestion() throws IOException {
         controller.doGet(request, response);
-
+        List<Question> questions = (List<Question>) request.getAttribute("questions");
+        Question question = questions.get(0);
+        assertEquals(question.getId(), 1L);
+        assertEquals(question.getDescription(), "Scrumの用語はどれか？");
     }
 }
